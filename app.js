3c3c51b333abe50ac0eb03e090e6e3348c2e50c5
naldurien <nicole.aldurien@gmail.com>
2021-04-23 23:05:37 -0400

    db connection

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
+// connection string
+const dbURI = 'mongodb+srv://nicolealdurien:MDBAthumper27@cluster0.nijve.mongodb.net/blogdb?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => console.log('connected to db'))
+.catch((err) => console.log(err))
 
 app.set('view engine', 'ejs')
 
